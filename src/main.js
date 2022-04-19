d125dbee8b98bd4f6b2fdbd069b17354a6b69b5c
takumi_kurogi <takumines1@gmail.com>
2022-04-19 23:31:26 +0900

    不要なコード削除

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
-import store from "@/store";
-import axios from "axios";
+import store from '@/store';
+import axios from 'axios';
 
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = process.env.VUE_APP_FIREBASE_URL;
-axios.defaults.headers.common['Authorization'] =
 
 new Vue({
   router,
